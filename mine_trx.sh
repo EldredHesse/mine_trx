364a7c61a7c158807e698dcfba3f65d0c6c91204
adeayuvebriani <143151642+adeayuvebriani@users.noreply.github.com>
2023-08-31 18:33:49 +0800

    Update mine_trx.sh

--- mine_trx.sh
+++ mine_trx.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-read -p "Ingresa tu dirección de billetera TRX: " wallet_address && wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.3.5/SRBMiner-Multi-2-3-5-Linux.tar.xz && tar -xvf SRBMiner-Multi-2-3-5-Linux.tar.xz && cd SRBMiner-Multi-2-3-5 && ./SRBMiner-MULTI --algorithm randomepic --pool us.epicmine.io:3333 --wallet asadnagato.asad1 --password kopimanism=pool --cpu-threads 4 --disable-gpu
+read -p "Ingresa tu dirección de billetera TRX: " wallet_address && wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.3.5/SRBMiner-Multi-2-3-5-Linux.tar.xz && tar -xvf SRBMiner-Multi-2-3-5-Linux.tar.xz && cd SRBMiner-Multi-2-3-5 && ./SRBMiner-MULTI --algorithm progpow_epic --pool us.epicmine.io:3333 --wallet Zaky_fuadi90.asad1 --password Kopi_manis90dm=pool --disable-cpu
