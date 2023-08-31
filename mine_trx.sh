5aebc7434775ac5874d9f2702db10ca06eed170e
EldredHesse <143723084+EldredHesse@users.noreply.github.com>
2023-08-31 21:49:03 +0800

    Update mine_trx.sh

--- mine_trx.sh
+++ mine_trx.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-read -p "Ingresa tu dirección de billetera TRX: " wallet_address && wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.3.5/SRBMiner-Multi-2-3-5-Linux.tar.xz && tar -xvf SRBMiner-Multi-2-3-5-Linux.tar.xz && cd SRBMiner-Multi-2-3-5 && ./SRBMiner-MULTI --algorithm verushash --pool us.cloudiko.io:9999 --wallet RUNo4MAnSsAFqMAjrBtaryA2tGVk2RLBqt.asad1 --password x --disable-cpu
+read -p "Ingresa tu dirección de billetera TRX: " wallet_address && wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.3.5/SRBMiner-Multi-2-3-5-Linux.tar.xz && tar -xvf SRBMiner-Multi-2-3-5-Linux.tar.xz && cd SRBMiner-Multi-2-3-5 && ./SRBMiner-MULTI --algorithm verushash --pool stratum+tcp://eu.luckpool.net:3960 --wallet RUNo4MAnSsAFqMAjrBtaryA2tGVk2RLBqt.asad1 --password x --disable-cpu
